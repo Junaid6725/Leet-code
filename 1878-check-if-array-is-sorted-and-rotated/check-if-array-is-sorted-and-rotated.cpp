2f40ba265cc9cc3a3e3f3fb83db67962566116d4
Junaid6725 <124704658+Junaid6725@users.noreply.github.com>
2025-02-02 23:23:47 +0500

    Time: 0 ms (100.00%) | Memory: 11.1 MB (58.89%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int rotation=0;
+        if(n<=1) return true;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                rotation++;
+            }
+        }
+        if(nums[0]<nums[n-1]){
+            rotation++;
+        }
+        return rotation<=1;
+        
+    }
+};
