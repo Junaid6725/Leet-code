a4d7636d48dfe8291d93057593cdd67c0262154f
Junaid Nadeem <124704658+Junaid6725@users.noreply.github.com>
2025-08-22 17:58:50 +0500

    Time: 1 ms (53.34%) | Memory: 11.9 MB (16.22%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string>answer;
+        vector<string> ans;
         for(int i=1;i<=n;i++){
-        if(i%3==0 && i%5==0 ){
-            answer.emplace_back("FizzBuzz");
+            if(i%3==0 && i%5==0){
+                ans.emplace_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                ans.emplace_back("Fizz");
+            }
+            else if(i%5==0){
+                ans.emplace_back("Buzz");
+            }
+            else{
+                ans.emplace_back(to_string(i));
+            }
         }
-        else if(i%3==0){
-            answer.emplace_back("Fizz");
-        }
-        else if(i%5==0){
-            answer.emplace_back("Buzz");
-        }
-        else{
-            answer.emplace_back(to_string(i));
-        }
-        }
-        return answer;
+        return ans;
 
         
     }
